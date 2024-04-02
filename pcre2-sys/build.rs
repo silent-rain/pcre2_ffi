a6caf64a12ba0f76dd39057616f6962fefe861a7
silent-rain <2367221387@qq.com>
2024-04-02 19:55:48 +0800

    优化pcre2绑定

--- pcre2-sys/build.rs
+++ pcre2-sys/build.rs
@@ -1,8 +1,10 @@
-extern crate bindgen;
+// extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
+
+use bindgen::{CargoCallbacks, EnumVariation};
 
 fn main() {
     // 获取项目的根目录
@@ -13,10 +15,8 @@ fn main() {
     download_pcre2(pcre2_dir.clone()).expect("Failed to clone PCRE2 repository");
     // 编译 PCRE2 项目
     build_pcre2(pcre2_dir.clone()).expect("Failed to build pcre2 project with cmake");
-    // 链接静态库 -  PCRE2
+    // 链接静态库
     link_pcre2(pcre2_dir.clone());
-    // 链接静态库 - PCRE2 Posix
-    link_pcre2_posix(pcre2_dir.clone());
     // 生成 Rust 绑定
     binding_pcre2(pcre2_dir);
 }
@@ -59,110 +59,14 @@ fn build_pcre2(pcre2_dir: PathBuf) -> std::io::Result<()> {
     Ok(())
 }
 
-/// 链接静态库 - PCRE2
+/// 链接静态库
 fn link_pcre2(pcre2_dir: PathBuf) {
-    // 指定静态库位置
-    let pcre2_path = pcre2_dir
-        .join("build")
-        .join("CMakeFiles")
-        .join(format!("{}.dir", "pcre2-8-static"))
-        .join("src");
-    // 检查对象文件价是否存在
-    if !pcre2_path.exists() {
-        panic!("Object file dir {:?} does not exist", pcre2_path);
-    }
-
-    // 创建静态库文件
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let lib_path = PathBuf::from(out_dir.clone()).join("libpcre2.a");
-    Command::new("ar")
-        .args([
-            "qc",
-            lib_path.to_str().unwrap(),
-            pcre2_dir
-                .parent()
-                .unwrap()
-                .join("pcre2_chartables.c.o")
-                .to_str()
-                .unwrap(),
-            pcre2_path.join("pcre2_auto_possess.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_chkdint.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_compile.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_config.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_context.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_convert.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_dfa_match.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_error.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_extuni.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_find_bracket.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_jit_compile.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_maketables.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_match.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_match_data.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_newline.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_ord2utf.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_pattern_info.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_script_run.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_serialize.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_string_utils.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_study.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_substitute.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_substring.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_tables.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_ucd.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_valid_utf.c.o").to_str().unwrap(),
-            pcre2_path.join("pcre2_xclass.c.o").to_str().unwrap(),
-        ])
-        .status()
-        .expect("Failed to create static library");
-
-    Command::new("ranlib")
-        .args([lib_path.to_str().unwrap()])
-        .status()
-        .expect("Failed to ranlib static library");
-
-    // 静态库的搜索路径
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    let lib_path = pcre2_dir.join("build");
+    // 搜索静态库路径
+    println!("cargo:rustc-link-search=native={}", lib_path.display());
     // 链接静态库
-    println!("cargo:rustc-link-lib=static=pcre2");
-}
-
-/// 链接静态库 - PCRE2 Posix
-fn link_pcre2_posix(pcre2_dir: PathBuf) {
-    // 指定静态库位置
-    let pcre2posix_o_path = pcre2_dir
-        .join("build")
-        .join("CMakeFiles")
-        .join(format!("{}.dir", "pcre2-posix-static"))
-        .join("src")
-        .join("pcre2posix.c.o");
-    // 检查对象文件是否存在
-    if !pcre2posix_o_path.exists() {
-        panic!("Object file {:?} does not exist", pcre2posix_o_path);
-    }
-
-    // 创建静态库文件
-    // ar qc target pcre2/build/CMakeFiles/pcre2-posix-static.dir/src/pcre2posix.c.o
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let lib_path = PathBuf::from(out_dir.clone()).join("libpcre2posix.a");
-    Command::new("ar")
-        .args([
-            "qc",
-            lib_path.to_str().unwrap(),
-            pcre2posix_o_path.to_str().unwrap(),
-        ])
-        .status()
-        .expect("Failed to create static library");
-
-    Command::new("ranlib")
-        .args([lib_path.to_str().unwrap()])
-        .status()
-        .expect("Failed to ranlib static library");
-
-    // 静态库的搜索路径
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    // 链接静态库
-    println!("cargo:rustc-link-lib=static=pcre2posix");
+    println!("cargo:rustc-link-lib=static=pcre2-8");
+    println!("cargo:rustc-link-lib=static=pcre2-posix");
 }
 
 /// 生成 Rust 绑定
@@ -173,6 +77,16 @@ fn binding_pcre2(pcre2_dir: PathBuf) {
     // 使用 bindgen 生成绑定
     let bindings = bindgen::Builder::default()
         .header(header_path.to_str().unwrap())
+        .generate_comments(true)
+        // u128 类型在 FFI（外部函数接口）中并不是安全的，因为它没有一个已知的稳定的 ABI（应用程序二进制接口）。
+        // 这意味着不同的编译器或不同的平台可能会以不同的方式处理这种类型，从而导致潜在的不兼容问题。
+        .blocklist_type("u128") // 阻止生成包含 u128 的绑定
+        .raw_line("pub type u128 = u64;") // u128 替换为 u64
+        .use_core()
+        .default_enum_style(EnumVariation::Rust {
+            non_exhaustive: true,
+        })
+        .parse_callbacks(Box::new(CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
